7252d332936eb9f0f010a8277ae262f755be9ea9
Lokky <lokky2789@gmail.com>
2023-02-22 21:00:19 +0300

    finbyid method added

--- src/main/java/org/newsportal/database/repository/impl/UserRepositoryImpl.java
+++ src/main/java/org/newsportal/database/repository/impl/UserRepositoryImpl.java
@@ -3,6 +3,7 @@ package org.newsportal.database.repository.impl;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.newsportal.database.repository.UserRepository;
 import org.newsportal.database.repository.entity.User;
 import org.newsportal.database.repository.util.HibernateUtil;
@@ -10,6 +11,7 @@ import org.newsportal.database.repository.util.HibernateUtil;
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.CriteriaUpdate;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
@@ -35,7 +37,16 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User findById(Long id) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<User> findUserByIdCriteriaQuery = builder.createQuery(User.class);
+            Root<User> root = findUserByIdCriteriaQuery.from(User.class);
+            findUserByIdCriteriaQuery.where(builder.equal(root.get("id"), id));
+            findUserByIdCriteriaQuery.select(root);
+            Query query = session.createQuery(findUserByIdCriteriaQuery);
+
+            return null;
+        }
     }
 
     @Override
@@ -45,21 +56,51 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User createUser(User user) {
+        try(Session session = sessionFactory.openSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaUpdate<User> saveUserCriteriaQuery = builder.createCriteriaUpdate(User.class);
+            Root<User> root = saveUserCriteriaQuery.from(User.class);
+
+
+        }
         return null;
     }
 
     @Override
     public User updateUserById(Long id, User user) {
+        try(Session session = sessionFactory.openSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaUpdate<User> updateUserCriteriaQuery = builder.createCriteriaUpdate(User.class);
+            Root<User> root = updateUserCriteriaQuery.from(User.class);
+            updateUserCriteriaQuery.set("username", user.getUsername());
+            updateUserCriteriaQuery.set("password", user.getPassword());
+            updateUserCriteriaQuery.where(builder.equal(root.get("id"), id));
+
+            Transaction transaction = session.beginTransaction();
+            session.createQuery(updateUserCriteriaQuery).executeUpdate();
+            transaction.commit();
+
+        }
         return null;
     }
 
     @Override
     public void deleteUserById(Long id) {
+        try(Session session = sessionFactory.openSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<User> criteriaQuery = builder.createQuery(User.class);
+            Root<User> root = criteriaQuery.from(User.class);
+            criteriaQuery.where(builder.equal(root.get("id"), id));
+
+            Transaction transaction = session.beginTransaction();
+            session.createQuery(criteriaQuery).executeUpdate();
+            transaction.commit();
+        }
 
     }
 
     public static void main(String[] args) {
         UserRepository userRepository = new UserRepositoryImpl();
-        System.out.println(userRepository.findAll());
+        System.out.println(userRepository.);
     }
 }
