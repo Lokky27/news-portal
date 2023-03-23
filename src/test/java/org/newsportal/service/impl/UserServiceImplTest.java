174b0981d9ce94ea60ceddfeae9acddf822c33af
Lokky <lokky2789@gmail.com>
2023-03-23 22:23:51 +0300

    All services are covered by tests

--- src/test/java/org/newsportal/service/impl/UserServiceImplTest.java
+++ src/test/java/org/newsportal/service/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package org.newsportal.service.impl;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -45,22 +46,34 @@ class UserServiceImplTest {
     @Test
     void getById() {
         userService.getById(anyLong());
-        verify(userMapper).mapToService((User) any());
+        verify(userMapper).mapToService((User) ArgumentMatchers.any());
         verify(repository).findById(anyLong());
     }
 
     @Test
     void getByUsername() {
+        userService.getByUsername(anyString());
+        verify(userMapper).mapToService((User) ArgumentMatchers.any());
+        verify(repository).findByUsername(anyString());
 
     }
 
     @Test
     void createUser() {
+        userService.createUser(ArgumentMatchers.any());
+        verify(userMapper).mapToDatabase((org.newsportal.service.model.User) ArgumentMatchers.any());
+        verify(repository).createUser(ArgumentMatchers.any());
 
     }
 
     @Test
     void changeUserById() {
+        org.newsportal.service.model.User updatableUser = new org.newsportal.service.model.User();
+        updatableUser.setUsername("Just a new User");
+        updatableUser.setPassword("A password of the new User");
+        userService.changeUserById(ArgumentMatchers.anyLong(), updatableUser);
+        verify(userMapper).mapToService((User) ArgumentMatchers.any());
+        verify(repository).updateUserById(ArgumentMatchers.anyLong(), ArgumentMatchers.any());
     }
 
     @Test
